d6b9f0c3e97615747ca6af0abfbefc67ca1b0ffe
Pawior <pawiorx@gmail.com>
2021-10-20 10:13:42 +0200

    deploy

--- book-visit-app-server/index.js
+++ book-visit-app-server/index.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const app = express();
-const http = require('http').createServer(app)
-const cors = require('cors')
-
-const dotenv = require('dotenv').config()
+const http = require("http").createServer(app);
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv").config();
 
 // ---- basic app and port configuration ----
-const PORT = process.env.NODE_SERVER_PORT
-app.use(express.static(__dirname + '/public'))
+const PORT = process.env.NODE_SERVER_PORT;
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(cors());
 
-//---------- routes ----------
-const emailActRoutes = require('./routes/emailSendRoutes')
-app.use(emailActRoutes)
+app.use(express.static(path.join(__dirname, "/book-visit-app/build")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/book-visit-app/build", "index.html"));
+});
+
+//---------- routes ----------
+const emailActRoutes = require("./routes/emailSendRoutes");
+app.use(emailActRoutes);
 
 // ---- listening ----
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
